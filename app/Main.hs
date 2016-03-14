48964426a0380354476e5396b1c40784470056e0
Helge Rausch <helge@rausch.io>
2016-03-14 09:28:57 +0100

    Return a string

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Environment (getArgs)
 import qualified Data.Map as Map
 import System.Random
+import Data.List (intercalate)
 
 type WordMap = Map.Map (String, String) [String]
 
@@ -13,7 +14,7 @@ main = do
     let wordLists = map words texts
     let wordMap = foldl addToWordMap Map.empty wordLists
     rng <- newStdGen
-    print $ take 10 $ generateText Nothing wordMap rng
+    print $ intercalate " " $ take 140 $ generateText Nothing wordMap rng
 
 addToWordMap :: WordMap -> [String] -> WordMap
 addToWordMap oldMap (w1:w2:w3:wordList) = addToWordMap newMap (w2:w3:wordList)
@@ -23,9 +24,10 @@ addToWordMap oldMap _ = oldMap
 
 generateText :: Maybe (String, String) -> WordMap -> StdGen -> [String]
 generateText Nothing wordMap rng = w1:w2:(generateText nextKey wordMap newRng)
-    where keyCount = length . Map.keys $ wordMap
+    where firstWords = Map.filterWithKey (\k _ -> (head . fst) k `elem` ['A'..'Z']) wordMap
+          keyCount = length . Map.keys $ firstWords
           (randIndex, newRng) = randomR (0, keyCount - 1) rng :: (Int, StdGen)
-          (w1, w2) = Map.keys wordMap !! randIndex
+          (w1, w2) = Map.keys firstWords !! randIndex
           nextKey = Just (w1, w2)
 
 generateText (Just key) wordMap rng
