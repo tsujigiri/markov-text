4cfd6c6ba3971a0903db660c9f6354e0af4faf44
Helge Rausch <helge@rausch.io>
2016-03-28 17:04:08 +0200

    Add a first test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.Hspec
+import Lib
+import System.Random
+
+main = hspec $ do
+    describe "One way sentence" $ do
+        it "returns the same sentence" $ do
+            rng <- newStdGen
+            take (length sentence) (generate rng [sentence]) `shouldBe` sentence
+            where sentence = "The quick brown fox jumps over the lazy dog."
